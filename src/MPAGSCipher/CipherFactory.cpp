9bbb97f12e229d460e34a7292c6f4002c98c596e
root <pieter.keenan@icloud.com>
2021-11-23 16:50:38 +0000

    Tidy comments

--- src/MPAGSCipher/CipherFactory.cpp
+++ src/MPAGSCipher/CipherFactory.cpp
@@ -1,30 +1,3 @@
-// #include <memory>
-// #include <string>
-
-// //#include "Cipher.hpp"
-// #include "CipherFactory.hpp"
-// //#include "CipherType.hpp"
-// #include "CaesarCipher.hpp"
-// #include "PlayfairCipher.hpp"
-// #include "VigenereCipher.hpp"
-
-// std::unique_ptr<Cipher> cipherFactory(const CipherType type, const std::string& key) {
-//     switch (type) {
-//         case CipherType::Caesar :
-//             return std::make_unique<CaesarCipher>(key);
-//             break;
-//         case CipherType::Playfair :
-//             return std::make_unique<PlayfairCipher>(key);
-//             break;
-//         case CipherType::Vigenere :
-//             return std::make_unique<VigenereCipher>(key);
-//             break;
-//     }
-//     // Incase some how get out of the switch cases?
-//     return std::unique_ptr<Cipher>{};
-// }
-
-
 #include <memory>
 #include <string>
 
